ba57d71238d0570da796fac71494bf0215e45db2
brae513 <braedonperini@gmail.com>
2020-10-20 22:57:57 -0500

    ban command

--- commands/ban.js
+++ commands/ban.js
@@ -3,7 +3,7 @@ module.exports = {
 	description: 'bans someone',
 	execute(message, args) {
 		try{
-			if(message.guild != null && message.guild.id === '599851762400362517'){
+			if(message.guild != null && message.guild.id === '599851762400362517' && (message.author.id == '161975669834776576' || message.author.id == '378445325351780366')){
 				if(!message.mentions.members.size){
 					message.channel.send("You need to mention someone for this command.");
 				}
@@ -11,7 +11,7 @@ module.exports = {
 					var targets = 0;
 					message.mentions.members.each(member =>{
 						var found = true;
-						console.log(member.voice);
+						console.log(member.voice.connection.status);
 						if(found){
 							targets += 1;
 							member.voice.setChannel('606679114564239361');
