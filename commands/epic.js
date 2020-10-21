b32ae1f86ea2c14ea3b521361d64d43028adeb7a
brae513 <braedonperini@gmail.com>
2020-10-20 22:39:45 -0500

    epic command

--- commands/epic.js
+++ commands/epic.js
@@ -8,9 +8,10 @@ module.exports = {
 		try{
 			if(message.guild != null && message.guild.id === '599851762400362517'){
 				message.guild.emojis.cache.each(emoji=>{
-					console.log(emoji.name+":"+emoji.id);
+					//console.log(emoji.name+":"+emoji.id);
 				});
-				message.channel.send(message.guild.emojis.cache.get('749519699062292540'));
+				//message.channel.send(message.guild.emojis.cache.get('749519699062292540'));
+				message.channel.send(":thatsepicemote:");
 			}
 			}catch(err){
 				console.log("Error in joke");
