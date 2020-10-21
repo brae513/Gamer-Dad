ac05c81d11a473dce0dd06f1e2a47ad8180c59ad
brae513 <braedonperini@gmail.com>
2020-10-20 22:52:38 -0500

    ban command

--- commands/ban.js
+++ commands/ban.js
@@ -1,11 +1,28 @@
-const fetch = require('node-fetch');
-
-
 module.exports = {
 	name: 'ban',
 	description: 'bans someone',
 	execute(message, args) {
 		try{
+			if(message.guild != null && message.guild.id === '599851762400362517'){
+				if(!message.mentions.members.size){
+					message.channel.send("You need to mention someone for this command.");
+				}
+				else{
+					var targets = 0;
+					message.mentions.members.each(member =>{
+						var found = true;
+						console.log(member.voice);
+						if(found){
+							targets += 1;
+							member.voice.setChannel('606679114564239361');
+						}
+					});
+					if(targets === 0){
+						message.channel.send("User not in a voice call");
+					}
+				}
+			}
+
 			/*if(!message.mentions.members.size){
 				message.reply(json.insult);
 			}
