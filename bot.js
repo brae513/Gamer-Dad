95ebcd448e74778ef3a8dab5484a0aa854b729e0
brae513 <braedonperini@gmail.com>
2020-10-20 20:54:09 -0500

    reactions

--- bot.js
+++ bot.js
@@ -17,6 +17,7 @@ client.on('ready', () => {
  
 
 client.on('message', message => {
+	console.log(message.author.discriminator);
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
