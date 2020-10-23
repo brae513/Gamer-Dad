343f858c53e858a9e21f3eee4149be1e29582da1
brae513 <braedonperini@gmail.com>
2020-10-23 15:59:19 -0500

    add korone random gifs

--- commands/korone.js
+++ commands/korone.js
@@ -6,7 +6,13 @@ module.exports = {
 	description: 'korone',
 	execute(message, args) {
 		try{
-			message.channel.send("https://tenor.com/view/inugami-korone-holo-live-fortnite-dance-meme-gif-17034919");
+			var gifs = [ "https://tenor.com/view/korone-pet-pet-gif-18248733",
+					"https://tenor.com/view/inugami-korone-hololive-thinking-anime-confused-gif-17902545",
+					"https://tenor.com/view/inugami-korone-gif-18362148",
+					"https://tenor.com/view/korone-hololive-inugami-korone-smile-dancing-gif-17703887",
+					"https://tenor.com/view/inugami-korone-holo-live-fortnite-dance-meme-gif-17034919"];
+			var selected = Math.floor(Math.random()*gifs.length);
+			message.channel.send(gifs[selected]);
 		}catch(err){
 			console.log(err.stack);
 		}
