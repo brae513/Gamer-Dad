14a6e7431af4bfdfa6d6ec03a65763e65cd25ab5
brae513 <braedonperini@gmail.com>
2020-10-26 16:51:09 -0500

    rtx

--- commands/rtx.js
+++ commands/rtx.js
@@ -7,10 +7,11 @@ module.exports = {
 	execute(message, args) {
 		try{
 			if(!rtx){
-				message.channel.send("RTX on");
+				message.channel.send("RTX on\n https://cdn.discordapp.com/attachments/618845562262913066/770403899605385256/mr-incredible.png");
+				
 			}
 			else{
-				message.channel.send("RTX off");
+				message.channel.send("RTX off\n https://cdn.discordapp.com/attachments/706003822601961502/768301698295791616/ffngdij9nv851.png");
 			}
 			rtx=!rtx;
 						
