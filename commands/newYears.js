038f0600662c2864cbc4693d89e6b454c4a785c1
brae513 <braedonperini@gmail.com>
2020-12-30 20:23:11 -0600

    New years

---
+++ commands/newYears.js
@@ -0,0 +1,27 @@
+const fetch = require('node-fetch');
+
+
+module.exports = {
+	name: 'epic',
+	description: 'that\'s epic',
+	execute(message, args) {
+		try{
+			var countDownDate = new Date("Jan 1, 2021 0:0:0").getTime();
+			var now = new Date().getTime();
+
+			// Find the distance between now and the count down date
+			var distance = countDownDate - now;
+
+			// Time calculations for days, hours, minutes and seconds
+			var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+			var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+			var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+			var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+			
+			var str = days + " days, " + hours + " hours, " + minutes + " minutes, " + seconds +" seconds remaining."
+			message.channel.send(str);
+		}catch(err){
+			console.log(err.stack);
+		}
+	},
+};
