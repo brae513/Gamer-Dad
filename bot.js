3d988ec21274f5950ef002a55ddb59b3ebedbff1
brae513 <braedonperini@gmail.com>
2020-12-30 21:19:52 -0600

    New years for Marc

--- bot.js
+++ bot.js
@@ -62,7 +62,7 @@ function newYearsReminder(){
 				guild.channels.resolve('618845562262913066').send(str);
 			});
 		}
-		else if(seconds ==0){
+		else if(seconds > -1){
 			client.guilds.fetch('599851762400362517').then(guild=>{
 				console.log("new years reminder");
 				guild.channels.resolve('599851762400362519').send("Happy new years gamers!\nWelcome to "+year);
