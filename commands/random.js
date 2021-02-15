26889c6178c0d5ce35d2867e68dbd3906eb24d70
brae513 <braedonperini@gmail.com>
2021-02-14 18:20:38 -0600

    random

--- commands/random.js
+++ commands/random.js
@@ -7,7 +7,7 @@ module.exports = {
 		try{
 			var commandNames = ['joke','korone','insult','epic','rtx','deafen','loshomies','newyears','cat'];
 			var selected = Math.floor(Math.random()*commandNames.length);
-			const command = require(commandNames[selected]+'.js');
+			const command = require('./'+commandNames[selected]+'.js');
 			command.execute(message,args);
 			
 		}catch(err){
