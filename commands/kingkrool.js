3c4cd6d33922e57624fb604e05b8f2720d789f2e
brae513 <braedonperini@gmail.com>
2021-02-19 19:47:00 -0600

    King Krool

--- commands/kingkrool.js
+++ commands/kingkrool.js
@@ -15,7 +15,7 @@ module.exports = {
 			"https://cdn.discordapp.com/attachments/671036045135183882/812499737906774026/lizard2.gif",
 			"https://cdn.discordapp.com/attachments/671036045135183882/812499740893773834/kingkroolvicroy.gif"];
 			var selected = Math.floor(Math.random()*gifs.length);
-			message.channel.send(gifs[selected]+" look out it's King Krool!");
+			message.channel.send(gifs[selected]+" look out it's King K Rool!");
 		}catch(err){
 			console.log(err.stack);
 		}
