f420fe2a60fc5eb7851d30da535aaf6d7dff80ed
brae513 <braedonperini@gmail.com>
2021-02-28 13:42:30 -0600

    cat

--- commands/cat.js
+++ commands/cat.js
@@ -1,5 +1,5 @@
 
-
+const delay = 1000;
 module.exports = {
 	name: 'cat',
 	description: 'cat',
@@ -7,7 +7,10 @@ module.exports = {
 	client:false,
 	execute(message, args) {
 		try{
-			message.channel.send(`https://i.imgur.com/PeKEqzb.png https://i.imgur.com/nGm0WuK.png https://i.imgur.com/XBi1feh.png https://i.imgur.com/GsFSPHz.png`);
+			message.channel.send('https://i.imgur.com/PeKEqzb.png');
+			setTimeout(() => { message.channel.send('https://i.imgur.com/nGm0WuK.png');},delay);
+			setTimeout(() => { message.channel.send('https://i.imgur.com/XBi1feh.png');},delay*2);
+			setTimeout(() => { message.channel.send('https://i.imgur.com/GsFSPHz.png');},delay*3);
 		}catch(err){
 			console.log(err.stack);
 		}
