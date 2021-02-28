cc6ab492b6edcbe6d0dcfdefb84766811f9e8068
brae513 <braedonperini@gmail.com>
2021-02-28 13:35:34 -0600

    cat

--- commands/cat.js
+++ commands/cat.js
@@ -7,7 +7,7 @@ module.exports = {
 	client:false,
 	execute(message, args) {
 		try{
-			message.channel.send(`https://cdn.discordapp.com/attachments/367834588753690655/810615657682698280/image1254.png https://cdn.discordapp.com/attachments/367834588753690655/810615658030301244/image2-10.png https://cdn.discordapp.com/attachments/367834588753690655/810615658273439834/image3-4.png https://cdn.discordapp.com/attachments/367834588753690655/810615658529161296/image4-4.png`);
+			message.channel.send(`https://i.imgur.com/PeKEqzb.png https://i.imgur.com/nGm0WuK.png https://i.imgur.com/XBi1feh.png https://i.imgur.com/GsFSPHz.png`);
 		}catch(err){
 			console.log(err.stack);
 		}
