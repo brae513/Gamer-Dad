db160251a58fdc89e88514dee95356da7dcf18e1
brae513 <braedonperini@gmail.com>
2021-03-08 01:04:39 -0600

    matpat

--- commands/matpat.js
+++ commands/matpat.js
@@ -7,8 +7,11 @@ module.exports = {
 	client:false,
 	execute(message, args) {
 		try{
-			if(Math.random()<0.1){
+			if(message.author.id === '586033322942267402'){
 				message.channel.send("https://media.discordapp.net/attachments/399408989684891651/816741136315449354/widemfwheart.gif");
+			}
+			else if(message.author.id === '147136628215775233'){
+				message.channel.send("https://media.discordapp.net/attachments/599851762400362519/816951913237053440/image0.gif");
 			}
 			else{
 				message.channel.send("https://cdn.discordapp.com/attachments/671036045135183882/814045111062233128/makesweet_1.gif");
