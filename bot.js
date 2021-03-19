4ac86ec10948e1f2a49fb94242263b8783fe6479
brae513 <braedonperini@gmail.com>
2021-03-18 22:14:32 -0500

    alex is online

--- bot.js
+++ bot.js
@@ -117,11 +117,13 @@ client.on('voiceStateUpdate',(oldState, newState) => {
 		if(newState.member.id === '161975669834776576'){
 			console.log(newState.connection);
 			if(newState.voiceChannel != undefined && oldState.voiceChannel === undefined){
+				console.log("Join?");
 				client.guilds.fetch('161976243233751040').then(guild =>{
 					guild.channels.resolve('190981291192090624').send("https://tenor.com/view/alex-alex-is-alex-is-online-discord-alex-discor-gif-18621584");
 				});
 			}
 			else if(newState.voiceChannel === undefined && oldState.voiceChannel != undefined){
+				console.log("Left?");
 				client.guilds.fetch('161976243233751040').then(guild =>{
 					guild.channels.resolve('190981291192090624').send("https://tenor.com/view/alex-alex-is-offline-alex-is-online-gif-19938100");
 				});
