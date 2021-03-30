6918f7d2289fecf5e86981176b3129f383ffe83a
brae513 <braedonperini@gmail.com>
2021-03-30 00:36:54 -0500

    say

--- commands/kingkrool.js
+++ commands/kingkrool.js
@@ -13,7 +13,8 @@ module.exports = {
 			"https://cdn.discordapp.com/attachments/671036045135183882/812499682013216808/lizard1.gif",
 			"https://cdn.discordapp.com/attachments/671036045135183882/812499684793778217/sadlizard.gif",
 			"https://cdn.discordapp.com/attachments/671036045135183882/812499737906774026/lizard2.gif",
-			"https://cdn.discordapp.com/attachments/671036045135183882/812499740893773834/kingkroolvicroy.gif"];
+			"https://cdn.discordapp.com/attachments/671036045135183882/812499740893773834/kingkroolvicroy.gif",
+			"https://media.discordapp.net/attachments/367834588753690655/825474843112898560/rool.gif"];
 			var selected = Math.floor(Math.random()*gifs.length);
 			message.channel.send(gifs[selected]+"\nLook out it's King K Rool!");
 		}catch(err){
--- commands/say.js
+++ commands/say.js
@@ -7,7 +7,7 @@ module.exports = {
 	client:false,
 	execute(message, args) {
 		try{
-			if(message.guild != null && message.guild.id === '599851762400362517' && message.author === '161975669834776576'){
+			if(message.guild != null && message.guild.id === '599851762400362517' && message.author.id === '161975669834776576'){
 				message.delete();
 				var string = "";
 				for(arg of args){
