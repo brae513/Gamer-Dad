d1f75edabc84a626c0fccab7e5173240cf4541e7
brae513 <braedonperini@gmail.com>
2021-05-07 20:07:48 -0500

    date testing

--- commands/date.js
+++ commands/date.js
@@ -8,7 +8,7 @@ module.exports = {
 	execute(message, args) {
 		try{
 			var date = new Date();
-			now.setHours(now.getHours()-8);
+			date.setHours(date.getHours()-8);
 			console.log("Month:"+date.getMonth()+"\tDate:"+date.getDate()+"\tHours:"+date.getHours());
 		}catch(err){
 			console.log(err.stack);
