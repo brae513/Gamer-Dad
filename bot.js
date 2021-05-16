ead76dc68cb51c171a2585d6eeae2aa447a8b0b8
brae513 <braedonperini@gmail.com>
2021-05-15 21:08:48 -0500

    database testing

--- bot.js
+++ bot.js
@@ -106,12 +106,12 @@ client.on('ready', () => {
 		profile.init(pool);
 		
 		pool.connect();
-		pool.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+		/*pool.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
 			if (err) throw err;
 			for (let row of res.rows) {
 				console.log(JSON.stringify(row));
 			}
-		});
+		});*/
 	} catch(err){
 		console.log("error in startup");
 		console.log(err.stack);
