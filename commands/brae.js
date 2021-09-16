0b5d061035c906fbeeea796c41e397f2ab12663b
Brae513 <braedonperini@gmail.com>
2021-09-15 20:27:34 -0500

    It's coding time

--- commands/brae.js
+++ commands/brae.js
@@ -1,14 +1,14 @@
 
 
 module.exports = {
-	name: 'marcsmom',
-	description: 'marc\'s mom?',
+	name: 'brae',
+	description: 'It\'s coding time',
 	random:true,
 	client:false,
 	execute(message, args) {
 		try{
 			message.channel.send("https://tenor.com/view/hacker-pc-meme-matrix-codes-gif-16730883");
-			message.channel.send("It's coding time");
+			message.channel.send("It\'s coding time");
 		}catch(err){
 			console.log(err.stack);
 		}
